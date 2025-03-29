3391d765b8ee4cf41404c80336214bfe75ac7002
ductaip <dtai858@gmail.com>
2025-03-29 22:06:57 +0700

    JWT: config jwt of nestjs and add token service

--- src/routes/auth/auth.controller.ts
+++ src/routes/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common'
 import { AuthService } from './auth.service'
-import { RegisterBodyDTO, RegisterResDTO } from './auth.dto'
+import { LoginBodyDTO, LoginResDTO, RegisterBodyDTO, RegisterResDTO } from './auth.dto'
 
 @Controller('auth')
 export class AuthController {
@@ -9,9 +9,16 @@ export class AuthController {
     // @SerializeOptions({ type: RegisterResDTO })
     @Post('register')
     async register(@Body() body: RegisterBodyDTO) {
-        console.log('....')
+        // console.log('register controller :....')
         const result = await this.authService.register(body)
 
         return new RegisterResDTO(result)
+    }
+
+    @Post('login')
+    async login(@Body() body: LoginBodyDTO) {
+        const result = await this.authService.login(body)
+
+        return new LoginResDTO(result)
     }
 }
