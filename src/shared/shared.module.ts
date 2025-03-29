33f3d0b5207862e49fc8d1ab492b3d1e88f93689
ductaip <dtai858@gmail.com>
2025-03-29 20:09:49 +0700

    add token service

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { Global, Module } from '@nestjs/common'
 import { PrismaService } from './services/prisma.service'
 import { HashingService } from './services/hashing.service'
+import { TokenService } from './services/token.service'
 
-const sharedServices = [PrismaService, HashingService]
+const sharedServices = [PrismaService, HashingService, TokenService]
 @Global()
 @Module({
-  providers: sharedServices,
-  exports: sharedServices,
+    providers: sharedServices,
+    exports: sharedServices,
 })
 export class SharedModule {}
