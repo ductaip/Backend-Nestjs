1f129a3b36b196e38998ed8c5d0f5f713ebf87d6
ductaip <dtai858@gmail.com>
2025-04-01 08:47:46 +0700

    combine authentication guard for auth

--- src/shared/guards/authentication.guard.ts
+++ src/shared/guards/authentication.guard.ts
@@ -7,16 +7,20 @@ import { APIKeyGuard } from 'src/shared/guards/api-key.guard'
 
 @Injectable()
 export class AuthenticationGuard implements CanActivate {
+    private authTypeGuardMap: Record<string, CanActivate>
+
     constructor(
         private readonly reflector: Reflector,
         private readonly accessTokenGuard: AccessTokenGuard,
         private readonly apiKeyGuard: APIKeyGuard,
-    ) {}
-    private readonly authTypeGuardMap: Record<string, CanActivate> = {
-        [AuthType.Bearer]: this.accessTokenGuard,
-        [AuthType.APIKey]: this.apiKeyGuard,
-        [AuthType.None]: { canActivate: () => true },
+    ) {
+        this.authTypeGuardMap = {
+            [AuthType.Bearer]: this.accessTokenGuard,
+            [AuthType.APIKey]: this.apiKeyGuard,
+            [AuthType.None]: { canActivate: () => true },
+        }
     }
+
     async canActivate(context: ExecutionContext): Promise<boolean> {
         const authTypeValue = this.reflector.getAllAndOverride<AuthTypeDecoratorPayload | undefined>(AUTH_TYPE_KEY, [
             context.getHandler(),
@@ -24,6 +28,7 @@ export class AuthenticationGuard implements CanActivate {
         ]) ?? { authTypes: [AuthType.None], options: { condition: ConditionGuard.And } }
         const guards = authTypeValue.authTypes.map((authType) => this.authTypeGuardMap[authType])
         let error = new UnauthorizedException()
+        console.log('authTypeValue', authTypeValue)
         if (authTypeValue.options.condition === ConditionGuard.Or) {
             for (const instance of guards) {
                 const canActivate = await Promise.resolve(instance.canActivate(context)).catch((err) => {
