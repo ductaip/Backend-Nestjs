35e80909fb30ad2193b42b5747a670f15a7456dc
ductaip <dtai858@gmail.com>
2025-04-09 09:33:50 +0700

    update sth

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import './shared/config'
 import { UnprocessableEntityException, ValidationPipe } from '@nestjs/common'
 import { LoggingInterceptor } from './shared/interceptor/logging.interceptor'
 import { TransformInterceptor } from './shared/interceptor/transform.interceptor'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
 
@@ -27,6 +28,16 @@ async function bootstrap() {
     )
     app.useGlobalInterceptors(new LoggingInterceptor())
     app.useGlobalInterceptors(new TransformInterceptor())
+
+    //swagger
+    const config = new DocumentBuilder()
+        .setTitle('Cats example')
+        .setDescription('The cats API description')
+        .setVersion('1.0')
+        .addTag('cats')
+        .build()
+    const documentFactory = () => SwaggerModule.createDocument(app, config)
+    SwaggerModule.setup('api', app, documentFactory)
 
     await app.listen(process.env.PORT ?? 3000)
 }
